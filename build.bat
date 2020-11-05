c99865df90b27d016e20e7336d5b00b054c3c3e1
RudolfRagbeer <djcpanis@gmail.com>
2020-11-05 17:04:28 +0100

    removed redundant actions, same as communication

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-mcrl22lps rocket.mcrl2 rocket.lps
+mcrl22lps rocket.mcrl2 rocket.lps --lin-method=regular2 --verbose
 lps2lts rocket.lps rocket.lts 
--- validate.bat
+++ validate.bat
@@ -1,3 +1,3 @@
-mcrl22lps rocket.mcrl2 rocket.lps
+mcrl22lps rocket.mcrl2 rocket.lps --lin-method=regular2 --verbose
 lps2pbes rocket.lps -f validation_rocket.mcf rocket.pbes
 pbes2bool rocket.pbes
