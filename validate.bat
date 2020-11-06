75331efea724ab4bccfed8be09d51c89f12e3d1c
Luc Dierick <ladierick@gmail.com>
2020-11-06 16:46:21 +0100

    added BM mcf req and OM mcf req

--- validate.bat
+++ validate.bat
@@ -1,6 +1,10 @@
-mcrl22lps -l stack rocket.mcrl2 rocket.lps
-lps2pbes -v -c -f validation_rocket.mcf rocket.lps rocket.pbes
-pbessolve -v --file=rocket.lps rocket.pbes
-pbes2bool rocket.pbes
-lps2lts rocket.pbes.evidence.lps rocket.pbes.evidence.lts
-ltsgraph rocket.pbes.evidence.lts
+@REM mcrl22lps -l stack rocket.mcrl2 rocket.lps
+@REM lps2pbes -v -c -f validation_rocket.mcf rocket.lps rocket.pbes
+@REM pbessolve -v --file=rocket.lps rocket.pbes
+@REM pbes2bool rocket.pbes
+@REM lps2lts rocket.pbes.evidence.lps rocket.pbes.evidence.lts
+@REM ltsgraph rocket.pbes.evidence.lts
+
+
+lps2pbes -c rocket.lps -f validation_rocket.mcf rocket.pbes
+pbes2bool rocket.pbes --file=rocket.lps --evidence-file=rocket-evidence.lps
